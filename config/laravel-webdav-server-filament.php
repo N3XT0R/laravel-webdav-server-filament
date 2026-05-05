6bd0ecdea215473c75899866f82910cd3b497aa9
Ilya Beliaev <info@php-dev.info>
2026-05-05 21:16:27 +0200

    chore(config): add password policy configuration and unit test suite

--- config/laravel-webdav-server-filament.php
+++ config/laravel-webdav-server-filament.php
@@ -5,4 +5,11 @@ return [
     'notifications' => [
         'enabled' => true,
     ],
+
+    'password' => [
+        'min_length'         => 16,   // minimum password length; also used for generated passwords
+        'require_mixed_case' => true, // require both upper- and lowercase letters
+        'require_numbers'    => true, // require at least one number
+        'require_symbols'    => true, // require at least one symbol
+    ],
 ];
