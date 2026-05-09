9b8cab572b1a62accca1abe02170624e20658e22
Ilya Beliaev <info@php-dev.info>
2026-05-09 12:27:08 +0200

    .

--- tests/Feature/Resources/UserWebDavAccountResourceTest.php
+++ tests/Feature/Resources/UserWebDavAccountResourceTest.php
@@ -164,7 +164,7 @@ final class UserWebDavAccountResourceTest extends DatabaseTestCase
     private function createAccount(User $user, array $attributes = []): WebDavAccountModel
     {
         return WebDavAccountModel::create([
-            'username' => $attributes['username'] ?? 'account-'.Str::uuid()->toString(),
+            'username' => $attributes['username'] ?? 'account-' . Str::uuid()->toString(),
             'password_encrypted' => $attributes['password_encrypted'] ?? Hash::make('secret'),
             'enabled' => $attributes['enabled'] ?? true,
             'user_id' => $attributes['user_id'] ?? $user->id,
